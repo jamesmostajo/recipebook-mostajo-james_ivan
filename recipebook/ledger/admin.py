2c54fced74df320c9a8a0c1efc7e9a3c69a67e3e
jamesmostajo <jipmostajo@gmail.com>
2024-03-14 18:19:02 +0800

    added profile models and author to recipe

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Profile, Ingredient
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
 # Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
 
 class RecipeIngredientAdmin(admin.TabularInline):
     model = RecipeIngredient
@@ -9,4 +16,16 @@ class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeIngredientAdmin]  
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField()
+    
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -13,6 +22,13 @@ class Ingredient(models.Model):
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        null=True,
+        default = None,
+        on_delete = models.CASCADE,
+        related_name="users"
+    )
 
     def __str__(self):
         return self.name
@@ -38,3 +54,4 @@ class RecipeIngredient(models.Model):
     # for debugging
     def __str__(self):
         return f'{self.quantity} of {self.ingredient} for {self.recipe}'
+
